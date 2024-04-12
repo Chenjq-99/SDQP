674b5252998a8cde93d259ba297f5c1e9123594b
Chenjq <chenjq991212@gmail.com>
2024-04-12 11:02:43 +0800

    Upload HW_4

--- example/sdqp_example.cpp
+++ example/sdqp_example.cpp
@@ -7,30 +7,50 @@ using namespace Eigen;
 
 int main(int argc, char **argv)
 {
-    int m = 7;
-    Eigen::Matrix<double, 3, 3> Q;
-    Eigen::Matrix<double, 3, 1> c;
-    Eigen::Matrix<double, 3, 1> x;        // decision variables
+    int m = 5;
+    Eigen::Matrix<double, 3, 3> Q, Q_prox;
+    Eigen::Matrix<double, 3, 1> c, c_prox;
+    Eigen::Matrix<double, 3, 1> x, x_hat;        // decision variables
     Eigen::Matrix<double, -1, 3> A(m, 3); // constraint matrix
     Eigen::VectorXd b(m);                 // constraint bound
 
-    Q << 2.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 2.0;
-    c << 1.2, 2.5, -10.0;
+    Q << 8.0, -6.0, 2.0, -6.0, 6.0, -3.0, 2.0, -3.0, 2.0;
 
-    A << 1.0, 0.0, 0.0,
-        0.0, 1.0, 0.0,
-        0.0, 0.0, 1.0,
-        -0.7, 0.5, 0.0,
-        0.5, -1.0, 0.0,
-        0.0, 0.13, -1.0,
-        0.1, -3.0, -1.3;
-    b << 10.0, 10.0, 10.0, 1.7, -7.1, -3.31, 2.59;
+    c << 1.0, 3.0, -2.0;
 
-    double minobj = sdqp::sdqp<3>(Q, c, A, b, x);
+    A << 0.0, -1.0, -2.0,
+        -1.0, 1.0, -3.0,
+        1.0, -2.0, 0.0,
+        -1.0, -2.0, -1.0,
+        3.0, 5.0, 1.0;
 
-    std::cout << "optimal sol: " << x.transpose() << std::endl;
-    std::cout << "optimal obj: " << minobj << std::endl;
-    std::cout << "cons precision: " << (A * x - b).maxCoeff() << std::endl;
+    b << -1.0, 2.0, 7.0, 2.0, -1.0;
+
+    x_hat << 0.0, -1.0, 2.0; // a feasible initial guess
+
+    // x = x_hat;
+
+    constexpr double eps = 1e-5;
+    constexpr int max_iterations = 50;
+    Eigen::MatrixXd Identity_3 = Eigen::MatrixXd::Identity(3, 3);
+
+    double rho = 1.0, tol = 1;
+    int iteration = 0;
+
+    while (tol > eps && iteration <= max_iterations) {
+        ++iteration;
+        Q_prox = Q + 1.0 / rho * Identity_3;
+        c_prox = c -  1.0 / rho * x_hat;
+        double minobj = sdqp::sdqp<3>(Q_prox, c_prox, A, b, x);
+        std::cout << "=================================" << std::endl;
+        std::cout << "iteration: " << iteration << endl;
+        std::cout << "optimal sol: " << x.transpose() << std::endl;
+        std::cout << "optimal obj: " << minobj << std::endl;
+        std::cout << "cons precision: " << (A * x - b).maxCoeff() << std::endl;
+        tol = (x - x_hat).norm() / std::max(1.0, x.norm());
+        rho = std::min(rho * 10, 1e6);
+        x_hat = x;
+    }
 
     return 0;
 }
